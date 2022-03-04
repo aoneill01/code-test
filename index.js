02af9e5a3124c16f64b9f99ffa5a614947fdbf41
Andy O'Neill <Andrew.X.Oneill.-ND@disney.com>
2022-03-04 11:15:59 -0500

    Improve tests

--- index.js
+++ index.js
@@ -41,16 +41,27 @@ const titles2 = [
   },
 ];
 
-console.log('Starting tests...')
+console.log("Starting tests...");
 
-const name1 = getName(titles1);
-console.assert(name1 === "Example 3", "Did not find system c", name1);
-
-const name2 = getName(titles2);
+// For titles1, it doesn't have system 'b', but it does have system 'c', so it should return 'Example 3'.
+// System 'a' is ignored.
+const test1 = getName(titles1);
 console.assert(
-  name2 === null,
-  "Did not fall back to null when no system found",
-  name2
+  test1 === "Example 3",
+  "Test 1: expected: Example 3, received:",
+  test1
 );
 
-console.log('Finished tests')
+// Reversing the order of titles1 should give the same result.
+const test2 = getName([...titles1].reverse());
+console.assert(
+  test2 === "Example 3",
+  "Test 2: expected: Example 3, received:",
+  test2
+);
+
+// For titles2, it does not have any of the specified systems, so it should return null.
+const test3 = getName(titles2);
+console.assert(test3 === null, "Test 3: expected: null, received:", test3);
+
+console.log("Finished tests");
